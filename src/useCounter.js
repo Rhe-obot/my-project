99bb728fc5ce0e58a2b0f1ef05940f07ec1247bc
Tominiyi Kolade <137675901+Rhe-obot@users.noreply.github.com>
2023-10-22 18:08:14 +0100

    Update useCounter.js

--- src/useCounter.js
+++ src/useCounter.js
@@ -4,17 +4,11 @@ const useCounter = (initialValue = 0) => {
   const [count, setCount] = useState(initialValue);
 
   const increment = () => {
-    if (count >= 50) {
-      throw new Error('Counter cannot be greater than 50');
-    }
-    setCount(count + 1);
+       setCount(count + 1);
   };
 
   const decrement = () => {
-    if (count <= -50) {
-      throw new Error('Counter cannot be less than -50'); 
-    }
-    setCount(count - 1);
+      setCount(count - 1);
   };
 
   const reset = () => {
